1a41e13509cc88eb62f5ec5c0b110db42252734c
Jan <edarieta09@gmail.com>
2023-11-19 17:52:56 -0500

    assertArraysEqual.js added

---
+++ assertArraysEqual.js
@@ -0,0 +1,19 @@
+const eqArrays = function(arr1, arr2) {
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    } else if (arr1.length !== arr2.length) {
+      return false;
+    }
+  }
+  return true;
+}
+
+const assertArraysEqual = function(assertArray1, assertArray2) {
+  eqArrays(assertArray1, assertArray2);
+  if (eqArrays) {
+    console.log(`✅✅✅ Assertion Passed: ${assertArray1} === ${assertArray2}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${assertArray1} !== + ${assertArray2}`);
+  }
+};
