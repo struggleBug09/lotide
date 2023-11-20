da737b355cab75e9f6d9a5a13b95b84e4ccfcd9e
Jan <edarieta09@gmail.com>
2023-11-19 19:08:59 -0500

    editted with eslint

--- flatten.js
+++ flatten.js
@@ -1,15 +1,15 @@
 const flatten = function(arrayToFlatten) {
-  let flattenedArr = []
+  let flattenedArr = [];
   for (let element of arrayToFlatten) {
-      if (Array.isArray(element)) {
-          for (let innerElement of element) {
-              flattenedArr.push(innerElement)
-          }
-      } else {
-          flattenedArr.push(element)
+    if (Array.isArray(element)) {
+      for (let innerElement of element) {
+        flattenedArr.push(innerElement);
       }
+    } else {
+      flattenedArr.push(element);
+    }
   }
   return flattenedArr;
-}
+};
 
-console.log(flatten([1, 2, [3, 4], 5, [6]])) // => [1, 2, 3, 4, 5, 6]
+console.log(flatten([1, 2, [3, 4], 5, [6]])); // => [1, 2, 3, 4, 5, 6]
