6bdca9eb845fa3401757eb3d0e1d7c9d6cda60d3
Jan <edarieta09@gmail.com>
2023-11-20 18:50:30 -0500

    added countLetter.js

---
+++ countLetters.js
@@ -0,0 +1,19 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const countLetters = function(stringInputs) {
+  let result = {};
+  for (let letter of stringInputs) {
+    if (result[letter] > 0) {
+      result[letter] += 1;
+    } else if (result[letter] !== " ") {
+      result[letter] = 1;
+    }
+  }
+  return result;
+}
