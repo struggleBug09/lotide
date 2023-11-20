f39ec14d6bf485df66193d7f9ed3f81f161f070b
Jan <edarieta09@gmail.com>
2023-11-19 19:06:12 -0500

    added flatten.js

---
+++ flatten.js
@@ -0,0 +1,15 @@
+const flatten = function(arrayToFlatten) {
+  let flattenedArr = []
+  for (let element of arrayToFlatten) {
+      if (Array.isArray(element)) {
+          for (let innerElement of element) {
+              flattenedArr.push(innerElement)
+          }
+      } else {
+          flattenedArr.push(element)
+      }
+  }
+  return flattenedArr;
+}
+
+console.log(flatten([1, 2, [3, 4], 5, [6]])) // => [1, 2, 3, 4, 5, 6]
