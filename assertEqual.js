6821783a651462a8c92c57b3be7f199641ddde98
Jan <edarieta09@gmail.com>
2023-11-20 19:28:14 -0500

    added letterPositions.js, maybe still to add checks for eslint

--- assertEqual.js
+++ assertEqual.js
@@ -1,4 +1,3 @@
-// FUNCTION IMPLEMENTATION
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
     console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
@@ -6,3 +5,4 @@ const assertEqual = function(actual, expected) {
     console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
   }
 };
+assertEqual(1, 1);
---
+++ letterPositions.js
@@ -0,0 +1,32 @@
+const assertArraysEqual = function(assertArray1, assertArray2) {
+  const arraysAreEqual = eqArrays(assertArray1, assertArray2);
+  if (arraysAreEqual) {
+    console.log(`✅✅✅ Assertion Passed: ${assertArray1} === ${assertArray2}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${assertArray1} !== ${assertArray2}`);
+  }
+};
+const eqArrays = function(arr1, arr2) {
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1.length !== arr2.length) {
+      return false;
+    } else if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const letterPositions = function(sentence) {
+  let result = {};
+  for (let i = 0; i < sentence.length; i++) {
+    if (Array.isArray(result[sentence[i]])) {
+      result[sentence[i]].push(i);
+    } else if (sentence[i] !== " ") {
+      result[sentence[i]] = [];
+      result[sentence[i]].push(i);
+    }
+  }
+  return result;
+};
+
