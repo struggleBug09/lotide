e835ec5516ac4d913f00f0e0692df39bde5b1ab2
Jan Edmark Arieta <edarieta09@gmail.com>
2023-11-26 14:07:42 -0500

    findKey

---
+++ findKey.js
@@ -0,0 +1,26 @@
+const findKey = function(object, callback) {
+  for (let key in object) {
+    if (callback(object[key])) {
+      return key;
+    }
+  }
+};
+
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const check = findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 2) // => "noma"
+
+assertEqual(check, "noma")
