4ee3bceb1baab2286638b31659e650dda0bc631c
Jan Edmark Arieta <edarieta09@gmail.com>
2023-12-07 23:08:36 +0000

    edited head

--- head.js
+++ head.js
@@ -4,7 +4,4 @@ const head = function(arr) {
   return arr[0]
 }
 
-
-// TEST CODE
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+module.exports = head;
