954d4d0a978b4207dba3de3d6d0e5b55e99224e8
Jan Edmark Arieta <edarieta09@gmail.com>
2023-12-08 00:06:33 +0000

    added eqArrays

--- eqArrays.js
+++ eqArrays.js
@@ -1,11 +1,3 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
 const eqArrays = function(arr1, arr2) {
   if (arr1.length !== arr2.length) {
     return false;
@@ -17,4 +9,6 @@ const eqArrays = function(arr1, arr2) {
   }
   return true;
 };
+
+module.exports = eqArrays;
 
---
+++ test/eqArraysTest.js
@@ -0,0 +1,6 @@
+const assertEqual = require('../assertEqual');
+const eqArrays = require('../eqArrays');
+
+assertEqual(eqArrays([1, 2, 3],[1, 2, 3]), true);
+assertEqual(eqArrays([11, 23, "greg"],[11, 23, "greg"]), true);
+assertEqual(eqArrays(["this", "is", "cool"],["this", "is", "cool"]), true);
