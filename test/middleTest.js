9f32df19cbfc007f2eb3f8075b1652d475113799
Jan Edmark Arieta <edarieta09@gmail.com>
2023-12-08 17:36:49 +0000

    refactored middle and tail to use mocha chai

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,8 +1,21 @@
-const eqArrays = require('../eqArrays');
+// const eqArrays = require('../eqArrays');
+// const assertArraysEqual = require('../assertArraysEqual')
+const assert = require('chai').assert;
 const middle = require('../middle');
-const assertArraysEqual = require('../assertArraysEqual')
 
-console.log(middle([1, 2, 3, 4])); // => [2, 3]
-console.log(middle([1, 2, 3, 4, 5, 6, 7])); // => [4]
-assertArraysEqual(middle([1, 2, 3, 4]),[2, 3]);
+describe("#middle", () => {
+  it("returns [2, 3] for [1, 2, 3, 4]", () => {
+    assert.deepEqual(middle([1, 2, 3, 4]),[2, 3]);
+  });
+  it("returns [4] for [1, 2, 3, 4, 5, 6, 7]", () => {
+    assert.deepEqual(middle([1, 2, 3, 4]),[2, 3]);
+  });
+  it("returns [10, 11] for [9, 10, 11, 12]", () => {
+    assert.deepEqual(middle([9, 10, 11, 12]),[10, 11]);
+  });
+});
+
+// console.log(middle([1, 2, 3, 4])); // => [2, 3]
+// console.log(middle([1, 2, 3, 4, 5, 6, 7])); // => [4]
+// assertArraysEqual(middle([1, 2, 3, 4]),[2, 3]);
 // console.log(eqArrays([1],[1]))
--- test/tailTest.js
+++ test/tailTest.js
@@ -1,6 +1,26 @@
-const assertEqual = require('../assertEqual');
+// const assertEqual = require('../assertEqual');
+const assert = require('chai').assert;
 const tail = require('../tail');
+
 const result = tail([5,6,7])
 
-assertEqual(result[0],6);
-assertEqual(result[1],7);
+describe("#tail", () => {
+  it("it returns [2,3] for [1,2,3]", () => {
+    assert.deepEqual(tail([1,2,3]), [2,3])
+  });
+  it("it returns returns [red, blue]", () => {
+    assert.deepEqual(tail(["yellow","red","blue"]), ["red","blue"])
+  });  
+});
+
+// describe("#head", () => {
+//   it("returns 1 for [1, 2, 3]", () => {
+//     assert.strictEqual(head([1, 2, 3]), 1);
+//   });
+//   it("returns '5' for ['5']", () => {
+//     assert.strictEqual(head(['5']), '5'); 
+//   });
+// });
+
+// assertEqual(result[0],6);
+// assertEqual(result[1],7);
