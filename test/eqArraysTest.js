d57ca31d422013e971696861d3d7aa7e1e7aa70b
Jan Edmark Arieta <edarieta09@gmail.com>
2023-12-08 01:39:59 +0000

    xd

--- test/eqArraysTest.js
+++ test/eqArraysTest.js
@@ -3,4 +3,4 @@ const eqArrays = require('../eqArrays');
 
 assertEqual(eqArrays([1, 2, 3],[1, 2, 3]), true);
 assertEqual(eqArrays([11, 23, "greg"],[11, 23, "greg"]), true);
-assertEqual(eqArrays(["this", "is", "cool"],["this", "is", "cool"]), true);
+assertEqual(eqArrays(["this", "is", "cool"],["this", "is", "terrible"]), false);
