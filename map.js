c531f542ee288798e96f18986a9c8b0c86b59fcf
Jan Edmark Arieta <edarieta09@gmail.com>
2023-12-21 04:28:11 +0000

    refactor takeUntil and map

--- map.js
+++ map.js
@@ -1,26 +1,3 @@
-const eqArrays = function(arr1, arr2) {
-  if (arr1.length !== arr2.length) {
-    return false;
-  }
-  for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) {
-      return false;
-    }
-  }
-  return true;
-}
-
-const assertArraysEqual = function(assertArray1, assertArray2) {
-  const arraysAreEqual = eqArrays(assertArray1, assertArray2);
-  if (arraysAreEqual) {
-    console.log(`✅✅✅ Assertion Passed: ${assertArray1} === ${assertArray2}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${assertArray1} !== ${assertArray2}`);
-  }
-};
-
-const numbers = [1,2,3,4,5];
-
 const map = function(array, callback) {
     const results = [];
     for (let item of array) {
@@ -28,8 +5,5 @@ const map = function(array, callback) {
   }
   return results;
 }
-const squaredNums = map(numbers, number => number * number);
-const doubledNums = map(numbers, number => number * 2);
-console.log("Doubled nums are: ", doubledNums);
-console.log("Squared nums are: ",squaredNums);
-assertArraysEqual(eqArrays(squaredNums,[ 1, 4, 9, 16, 25 ]), true)
+
+module.exports = map;
--- takeUntil.js
+++ takeUntil.js
@@ -9,27 +9,4 @@ const takeUntil = function(array, callback) {
   return result;
 }
 
-const eqArrays = function(arr1, arr2) {
-  if (arr1.length !== arr2.length) {
-    return false;
-  }
-  for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) {
-      return false;
-    }
-  }
-  return true;
-};
-const arr = [-3, -2, -1, 0, 1, 2, 3]
-const onlyNegative = takeUntil(arr, number => number < 0)  //return array with only negative
-
-const assertArraysEqual = function(assertArray1, assertArray2) {
-  const arraysAreEqual = eqArrays(assertArray1, assertArray2);
-  if (arraysAreEqual) {
-    console.log(`✅✅✅ Assertion Passed: ${assertArray1} === ${assertArray2}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${assertArray1} !== ${assertArray2}`);
-  }
-};
-
-assertArraysEqual(eqArrays(onlyNegative, [-3, -2, -1 ]), true)
+module.exports = takeUntil;
